bf9baaf5622981e0f21b1c2cf591068314365dc9
Fumito <yamagic202008@gmail.com>
2024-02-12 17:57:04 +0900

    feat:add Header component

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Noto_Sans_JP } from "next/font/google";
 import "./globals.css";
+import Header from "./components/Header";
 
 const notoSansJP = Noto_Sans_JP({ subsets: ["latin"], weight: ["400"] });
 
@@ -16,6 +17,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="jp">
+      <Header />
       <body className={notoSansJP.className}>{children}</body>
     </html>
   );
