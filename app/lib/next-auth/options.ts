c5b986a4eaddbfc9797a52d57441ac5448c76918
Fumito <yamagic202008@gmail.com>
2024-02-23 17:27:23 +0900

    feat:add prisma

---
+++ app/lib/next-auth/options.ts
@@ -0,0 +1,27 @@
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import GitHubProvider from "next-auth/providers/github";
+
+import { NextAuthOptions } from "next-auth";
+
+export const nextAuthOptions: NextAuthOptions = {
+  debug: true,
+  providers: [
+    GitHubProvider({
+
+      clientId: process.env.GITHUB_ID!,
+      clientSecret: process.env.GITHUB_SECRET!,
+    }),
+  ],
+  adapter: PrismaAdapter(prisma),
+  callbacks: {
+    session: ({ session, user }) => {
+      return {
+        ...session,
+        user: {
+          ...session.user,
+          id: user.id,
+        },
+      };
+    },
+  },
+};
