3a59b6a9d6f5ab7d7d5a4e184e6c818d91813c87
Fumito <yamagic202008@gmail.com>
2024-02-26 00:07:58 +0900

    feat:Obtain login user session information

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Noto_Sans_JP } from "next/font/google";
 import "./globals.css";
 import Header from "./components/Header";
+import { NextAuthProvider } from "./lib/next-auth/provider";
 
 const notoSansJP = Noto_Sans_JP({ subsets: ["latin"], weight: ["400"] });
 
@@ -18,8 +19,10 @@ export default function RootLayout({
   return (
     <html lang="jp">
       <body className={notoSansJP.className}>
-      <Header />
-      {children}
+        <NextAuthProvider>
+           <Header />
+           {children}
+        </NextAuthProvider>
       </body>
     </html>
   );
