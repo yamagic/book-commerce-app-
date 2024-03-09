55b9ef724f954ebb3a3f0539568bd945e17e7405
Fumito <yamagic202008@gmail.com>
2024-03-09 23:12:40 +0900

    feat:add stripe

---
+++ app/api/checkout/route.ts
@@ -0,0 +1,30 @@
+import { RouterChangeByServerResponse } from './../../../node_modules/next/dist/client/components/router-reducer/router-reducer-types.d';
+import Stripe from "stripe";
+
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!);
+
+export async function POST(request: Request, response:Response) {
+
+  const { title, price } = await request.json();
+
+  try {
+    const session = await stripe.checkout.sessions.create({
+      payment_method_types:["card"],
+      line_items:[
+        {
+          price_data: {
+            currency: "jpy",
+            product_data: {
+              name: title,
+            },
+            unit_amount: price,
+          },
+          quantity: 1,
+        }
+      ],
+      mode: "payment",
+      success_url: 'http://localhost:3000/book/checkout-success?session_id={CHECKOUT_SESSION_ID}',
+      cancel_url: "http://localhost:3000"
+    })
+  } catch{}
+}
