7530c39fa5cd22cac3cc14f40fedca58bcd6364a
Fumito <yamagic202008@gmail.com>
2024-03-20 14:22:09 +0900

    feat:電子記事詳細ページの表示

--- app/book/[id]/page.tsx
+++ app/book/[id]/page.tsx
@@ -1,27 +1,30 @@
+import { getDetailBook } from "@/app/lib/microcms/client";
 import Image from "next/image";
 import React from "react";
 
-const DetailBook = async () => {
+const DetailBook = async ({params}: { params: { id: string } }) => {
+  const book = await getDetailBook(params.id)
+
   return (
     <div className="container mx-auto p-4">
       <div className="bg-white shadow-lg rounded-lg overflow-hidden">
         <Image
-          src={""}
-          alt="book image"
+          src={book.thumbnail.url}
+          alt={book.title}
           className="w-full h-80 object-cover object-center"
           width={700}
           height={700}
         />
         <div className="p-4">
-          <h2 className="text-2xl font-bold">Test Book</h2>
+          <h2 className="text-2xl font-bold">{book.title}</h2>
           <div
             className="text-gray-700 mt-2"
-            dangerouslySetInnerHTML={{ __html: "<h1>Test</h1>" }}
+            dangerouslySetInnerHTML={{ __html: book.content }}
           />
 
           <div className="flex justify-between items-center mt-2">
-            <span className="text-sm text-gray-500">公開日:</span>
-            <span className="text-sm text-gray-500">最終更新:</span>
+            <span className="text-sm text-gray-500">公開日: {new Date(book.publishedAt as any).toLocaleString()}</span>
+            <span className="text-sm text-gray-500">最終更新: {new Date(book.updatedAt).toLocaleString()}</span>
           </div>
         </div>
       </div>
