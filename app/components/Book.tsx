62320030ce7e9fb8077ba0ca8b4cd3a50fba0e79
Fumito <yamagic202008@gmail.com>
2024-03-20 15:33:11 +0900

    feat:購入済みの商品を購入できないように

--- app/components/Book.tsx
+++ app/components/Book.tsx
@@ -9,10 +9,11 @@ import { BookType } from "../types/types";
 
 type BookProps = {
   book: BookType;
+  isPurchased: boolean;
 };
 
 // eslint-disable-next-line react/display-name
-const Book = ({ book }: BookProps) => {
+const Book = ({ book, isPurchased }: BookProps) => {
   const [showModal, setShowModal] = useState(false);
   const { data: session } = useSession();
   const user: any = session?.user;
@@ -45,7 +46,11 @@ const Book = ({ book }: BookProps) => {
   }
 
   const handleParchaseClick = () => {
-    setShowModal(true)
+    if(isPurchased) {
+      alert("その商品は購入済みです。")
+    } else {
+      setShowModal(true)
+    }
   }
 
   const handleCansel = () => {
--- app/page.tsx
+++ app/page.tsx
@@ -11,12 +11,18 @@ export default async function Home() {
   const session = await getServerSession(nextAuthOptions)
   const user: any = session?.user
 
+  let purchaseBookIds: any
+
   if(user) {
     const response = await fetch(
-      `${process.env.NEXT_PUBLIC_API_URL}/purchases/${user.id}`
+      `${process.env.NEXT_PUBLIC_API_URL}/purchases/${user.id}`,
+      { cache: "no-store"}
     )
     const purchasesData = await response.json()
-    console.log(purchasesData)
+
+    purchaseBookIds = purchasesData.map(
+      (purchaseBook: any) => purchaseBook.bookId
+    )
   }
 
   return (
@@ -26,7 +32,7 @@ export default async function Home() {
           Book Commerce
         </h2>
         {contents.map((book:BookType) => (
-          <Book key={book.id} book={book} />
+          <Book key={book.id} book={book} isPurchased={purchaseBookIds.includes(book.id)}/>
         ))}
       </main>
     </>
