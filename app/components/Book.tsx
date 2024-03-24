545ce1a35686b35a4871870becc575a081042b78
Fumito <yamagic202008@gmail.com>
2024-03-24 20:20:21 +0900

    feat:urlを環境変数で定義するように

--- app/components/Book.tsx
+++ app/components/Book.tsx
@@ -61,7 +61,7 @@ const Book = ({ book, isPurchased }: BookProps) => {
     if(!user) {
       setShowModal(false)
       // ログインページにリダイレクト
-      router.push("/login")
+      router.push("/api/auth/signin")
     } else {
       // stripeで決済する。
       startCheckout()
